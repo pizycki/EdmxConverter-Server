7cbb08581526b8ec98604c834a382dedfb5aa3cb
pizycki <pawelizycki@gmail.com>
2017-09-26 00:05:37 +0200

    Add sources and targets

--- src/EdmxConv.WebAPI/Controllers/ConvertController.cs
+++ src/EdmxConv.WebAPI/Controllers/ConvertController.cs
@@ -2,6 +2,7 @@
 using EdmxConv.Behaviours;
 using EdmxConv.Schema.DTO;
 using System.Web.Http;
+using EdmxConv.Schema;
 
 namespace EdmxConv.WebAPI.Controllers
 {
@@ -9,11 +10,28 @@ namespace EdmxConv.WebAPI.Controllers
     public class ConvertController : BaseApiController
     {
         [HttpPost, Route("")]
-        public IHttpActionResult Post([FromBody] ConvertParams payload) =>
+        public IHttpActionResult Convert([FromBody] ConvertParams payload) =>
             ConvertParamsValidationModule.Validate(payload)
                 .OnSuccess(p => ConvertEdmxArgsModule.CreateArguments(p))
                 .OnSuccess(arg => ConvertModule.Convert(arg))
                 .Map(edmx => edmx.ToString())
                 .OnBoth(MapToHttpResponse);
+
+        [HttpGet, Route("targets")]
+        public IHttpActionResult Targets() => Ok(new dynamic[]
+        {
+            new { Name = "XML",      Type = EdmxTypeEnum.Xml },
+            new { Name = "Resource", Type = EdmxTypeEnum.Resource },
+            new { Name = "Database", Type = EdmxTypeEnum.Database }
+        });
+
+        [HttpGet, Route("sources")]
+        public IHttpActionResult Sources() => Ok(new dynamic[]
+        {
+            new { Name = "XML",      Type = EdmxTypeEnum.Xml },
+            new { Name = "Resource", Type = EdmxTypeEnum.Resource },
+            new { Name = "Database", Type = EdmxTypeEnum.Database }
+        });
+
     }
 }
